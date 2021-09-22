99e8086c6ebbe4bc92b81daa79a81bc7594c66bd
Ella Ahlstedt <ella.ahlstedt@learnet.se>
2021-09-22 13:51:38 +0200

    Added Start Game page and updated CSS

--- js/test2.js
+++ js/test2.js
@@ -3,7 +3,6 @@ const pages = [
         page: 1,
         title: "Introduction",
         text: "You’re a wizard in a group of adventures undertaking quests. When you awake in your inn one day, after a particularly gruelling quest, you find a letter claiming to be written by yourself even though you have no memory of writing such a thing. In the letter there is a strange artifact that can allegedly bring anyone who activates it back in time. The letter claims that you must go and retrieve the same artifact from where it was originally stored and then travel back in time to put together this letter in order for the space-time continuum to remain stable.",
-        
     },
     {
         page: 2,
@@ -31,11 +30,15 @@ const pages = [
     }
 ];
 
-console.log( returnIndexOfPage(1));
-let currentChoices = pages[1].choices;
-console.log(currentChoices);
-console.log( 'from room 1 you can move to' );
-currentChoices.forEach( choice => console.log(choice.browseto));
+
+// console.log( 'from room 1 you can move to' );
+// console.log( returnIndexOfPage(1));
+
+// let currentChoices = pages[1].choices;
+// console.log(currentChoices);
+// currentChoices.forEach( choice => console.log(choice.browseto));
+
+pagesnumb = 0;
 
 
 function returnIndexOfPage( pagenr ) {  return pages[pagenr].page; };
@@ -45,7 +48,35 @@ document.getElementById('pageNrRight').innerHTML = returnIndexOfPage(2);
 
 
 function returnChapterTitle( chaptitle ) {  return pages[chaptitle].title; };
+document.getElementById('pageTitle').innerHTML = returnChapterTitle(0);
 
-// document.getElementById('pageTitle').innerHTML = returnChapterTitle(1);
+function returnpageText( pageText ) {  return pages[pageText].text; };
+document.getElementById('text').innerHTML = returnpageText(0);
+
+function returnChoices( pageChoices, choicenr ) {  return pages[pageChoices].choices[choicenr].text; };
+document.getElementById('choice1').innerHTML = returnChoices(1,0);
+
+function returnChoices( pageChoices, choicenr ) {  return pages[pageChoices].choices[choicenr].text; };
+document.getElementById('choice2').innerHTML = returnChoices(1,1);
 
 
+
+// for (var i = 0; i < pages[2].choices.length; i++) {
+//     console.log(i + 1 + ". " + pages.choices[i].text);
+// }
+
+// for (var i = 0; i < scene.options.length; i++) {
+//     console.log(i + 1 + ". " + scene.options[i].text);
+// }
+
+// $(document).ready(function(){
+//     $("#choice1").click(function() {
+//       $('body').toggleClass("test");
+//     });
+//   })
+
+  function changePage() {
+
+  }
+
+//   if button is clicked = changes scene to another depening on choice
